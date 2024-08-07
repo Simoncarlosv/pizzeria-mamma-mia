b674810ea46df5ea8d1b4b8535bc9934a83c4b09
Simoncarlosv <simoncarlosv@gmail.com>
2024-08-07 18:26:22 -0400

    Agregamos todos los archivos

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  return(
+    <>
+      <div>
+        <Navbar/>
+        <Home/>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App;
